0fb11c101ab0c5a0d457d6e69291d49306de0033
DiMiRka <fallen_94@bk.ru>
2024-03-26 14:54:34 +1000

    Change 1

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,1 +1,8 @@
 print("Давайте же начнём игру!")
+field = [['-'] * 3 for i in range(3)]
+for i, row in enumerate(field):
+    row_str = str(i) + ' ' + ' '.join(map(str, row))
+    print(row_str)
+print('Начинают крестики')
+
+#while True:
