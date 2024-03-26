ca04747662480d8f1b2744b03975cb842bd8c1a2
DiMiRka <fallen_94@bk.ru>
2024-03-26 15:27:20 +1000

    Change 2

--- tic tac toe.py
+++ tic tac toe.py
@@ -4,5 +4,16 @@ for i, row in enumerate(field):
     row_str = str(i) + ' ' + ' '.join(map(str, row))
     print(row_str)
 print('Начинают крестики')
-
-#while True:
+while True:
+    move_cross = (list(map(int, input('Введите номер столбца и строки через пробел соответствено: ').split())))
+    one, two = move_cross[0], move_cross[1]
+    field[one][two] = 'x'
+    for i, row in enumerate(field):
+        row_str = str(i) + ' ' + ' '.join(map(str, row))
+        print(row_str)
+    move_zero = (list(map(int, input('Введите номер столбца и строки через пробел соответствено: ').split())))
+    one, two = move_zero[0], move_zero[1]
+    field[one][two] = 'o'
+    for i, row in enumerate(field):
+        row_str = str(i) + ' ' + ' '.join(map(str, row))
+        print(row_str)
