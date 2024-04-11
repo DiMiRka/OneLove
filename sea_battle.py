cbd6e8b462e05b85086236c6a763d064cdac1c7c
DiMiRka <fallen_94@bk.ru>
2024-04-11 17:34:23 +1000

    New_game Change_3

--- sea_battle.py
+++ sea_battle.py
@@ -50,7 +50,7 @@ class Board:  # Класс игрового поля
     def __init__(self, hid=False, size = 6):
         self.hid = hid  # Отображение кораблей на поле отображаются/не отображаются (True/False)
         self.size = size  # Размер корабля
-        self.wrecked_ships = 0  # Пораженные корабли
+        self.living_ships = 0  # Живые корабли
         self.field = [['O'] * size for i in range(size)]  # Расчерчивает поле
         self.busy = []  # Список занятых клеток (кораблями или ранее сделанными выстрелами)
         self.ships = []  # Список кораблей на поле
@@ -61,6 +61,67 @@ class Board:  # Класс игрового поля
         for i, row in enumerate(self.field):  # Чертим поле
             draw += f'\n {i+1} | ' + ' | '.join(row) + ' | '
 
-        if self.hid:  # Проверяем показывать ли корабли на поле
+        if self.hid:  # Проверяем прячем ли корабли на поле
             draw = draw.replase('■', 'O')
         return draw
+
+    def out(self, b):  # Проверяем выходят ли координаты за поле
+        return not (0 <= b.x < self.size) and (0 <= b.y < self.size)
+
+    def contour(self, ship, look=False):  # Делаем контур корабля + соседние от него клетки
+        near = [
+            (-1, -1), (0, -1), (1, -1),
+            (-1, 0), (0, 0), (1, 0),
+            (-1, 1), (0, 1), (1, 1)
+        ]
+        for i in ship.dots:
+            for ix, iy in near:
+                crd = Dot(i.x + ix, i.y + iy)
+                if crd not in self.busy and not self.out(crd):
+                    if look:
+                        self.field[crd.x][crd.y] = '·'
+                    self.busy.append(crd)
+
+    def add_ship(self, ship):  # Добавляем корабль на поле
+        for i in ship.dots:
+            if self.out(i) or i in self.busy:
+                raise BoardWrongShipExceptions()
+            else:
+                self.field[i.x][i.y] = '■'
+                self.busy.append(i)
+        self.ships.append(ship)
+        self.contour(ship)
+        self.living_ships += 1
+    def shot(self, c):  # Делаем выстрел
+        if self.out(c):  # Проверяем не выходит ли выстрел за поле
+            raise BoardOutExceptions()
+
+        if c in self.busy:  # Проверяем не стреляли ли в эту клетку ранее
+            raise BoardUsedExceptions()
+
+        self.busy.append(c)  # Добавляем выстрел в занятую клетку
+
+        for ship in self.ships:
+            if c in ship.shoots(c):
+                ship.heart -= 1
+                self.field[c.x][c.y] = 'X'
+                print('Корабль ранен')
+                if ship.heart == 0:
+                    self.living_ships -= 1
+                    self.contour(ship, look=True)
+                    print('Корабль уничтожен!')
+                    return False
+            else:
+                self.field[c.x][c.y] = '·'
+                print('Мимо!')
+                return True
+
+
+
+
+ship_1 = Ship(Dot(3, 3), 2, 0)
+board_1 = Board()
+board_1.add_ship(ship_1)
+board_1.contour(ship_1)
+
+print(board_1)
