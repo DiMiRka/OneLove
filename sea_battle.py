7e8be124010d59e45bcb774ac56eb9dab1b020a5
DiMiRka <fallen_94@bk.ru>
2024-04-12 21:12:15 +1000

    New_game Change_5

--- sea_battle.py
+++ sea_battle.py
@@ -1,6 +1,6 @@
 from random import randint
 
-class Exceptions:
+class Exceptions (Exception):
     pass
 
 class BoardOutExceptions(Exceptions):
@@ -25,16 +25,16 @@ class Dot:  # Класс координат (точки x и y)
         return f'Координата x: {self.x}, Координата y: {self.y}'
 
 class Ship:  # Класс кораблей
-    def __init__(self, ship_nose, size, direction):
-        self.size = size  # Длина корабля
+    def __init__(self, ship_nose, ship_size, direction):
         self.ship_nose = ship_nose  # Нос корабля (объект класса Dot)
+        self.ship_size = ship_size  # Длина корабля
         self.direction = direction  # Направление корабля вертикальное/горизонтальное (0/1)
-        self.heart = size  # Количество жизней корабля
+        self.heart = ship_size  # Количество жизней корабля
 
     @property
     def dots(self):  # Координаты корабля
         ship_dots = []
-        for i in range(self.size):
+        for i in range(self.ship_size):
             dot_x = self.ship_nose.x
             dot_y = self.ship_nose.y
             if self.direction == 0:
@@ -49,12 +49,12 @@ class Ship:  # Класс кораблей
 
 
 class Board:  # Класс игрового поля
-    def __init__(self, hid=False, size = 6):
-        self.hid = hid  # Отображение кораблей на поле отображаются/не отображаются (True/False)
-        self.size = size  # Размер корабля
+    def __init__(self, hid=False, size=6):
+        self.hid = hid  # Скрытие кораблей на поле (True/False)
+        self.size = size  # Размер поля
         self.living_ships = 0  # Живые корабли
-        self.field = [['O'] * size for i in range(size)]  # Расчерчивает поле
-        self.busy = []  # Список занятых клеток (кораблями или его контуром)
+        self.field = [['O'] * size for _ in range(size)]  # Расчерчивает поле
+        self.busy = []  # Список занятых клеток (кораблями и его контуром)
         self.ships = []  # Список кораблей на поле
         self.shots_taken = []  # Список сделанных выстрелов
 
@@ -64,14 +64,14 @@ class Board:  # Класс игрового поля
         for i, row in enumerate(self.field):  # Чертим поле
             draw += f'\n {i+1} | ' + ' | '.join(row) + ' | '
 
-        if self.hid:  # Проверяем прячем ли корабли на поле
+        if self.hid:  # Проверяем скрываем ли корабли на поле
             draw = draw.replase('■', 'O')
         return draw
 
     def out(self, b):  # Проверяем выходят ли координаты за поле
-        return not (0 <= b.x < self.size) and (0 <= b.y < self.size)
+        return not ((0 <= b.x < self.size - 1) and (0 <= b.y < self.size - 1))
 
-    def contour(self, ship, look=False):  # Делаем контур корабля + соседние от него клетки
+    def contour(self, ship, look=False):  # Делаем контур корабля
         near = [
             (-1, -1), (0, -1), (1, -1),
             (-1, 0), (0, 0), (1, 0),
@@ -80,11 +80,11 @@ class Board:  # Класс игрового поля
         for i in ship.dots:
             for ix, iy in near:
                 crd = Dot(i.x + ix, i.y + iy)
-                if crd not in self.busy and not self.out(crd):
+                if crd not in self.busy and not (self.out(crd)):
+                    if look and self.field[crd.x][crd.y] != 'X':
+                        self.field[crd.x][crd.y] = '·'
+                        self.shots_taken.append(crd)
                     self.busy.append(crd)
-                if look and self.field[crd.x][crd.y] != 'X':
-                    self.field[crd.x][crd.y] = '·'
-                    self.shots_taken.append(crd)
 
     def add_ship(self, ship):  # Добавляем корабль на поле
         for i in ship.dots:
@@ -120,10 +120,13 @@ class Board:  # Класс игрового поля
                 print('Мимо!')
                 return True
 
+    def begin(self):
+        self.busy = []
+
 class Player:
     def __int__(self, board, board_2):
-        self.board = Board()
-        self.board_2 = Board(hid=True)
+        self.board = board
+        self.board_2 = board_2
 
     def ask(self):
         pass
@@ -133,18 +136,20 @@ class Player:
             try:
                 target = self.ask()
                 repeat = self.board_2.shot(target)
-            except Exceptions as e:
+                return repeat
+            except Exception as e:
                 print(e)
 
 class AI (Player):
     def ask(self):
         d = Dot(randint(0, 5), randint(0, 5))
         print(f'Ход компьютера: {d.x + 1} {d.y + 1}')
+        return d
 
 class User (Player):
     def ask(self):
         while True:
-            d =  input('Введите координаты выстрела через пробел: ').split()
+            d = input('Введите координаты выстрела через пробел: ').split()
 
             if len(d) != 2: # Проверяем, что введены 2 координаты
                 print('Введите 2 координаты через пробел! ')
@@ -160,17 +165,34 @@ class User (Player):
             return Dot(x - 1, y - 1)
 
 class Game:
-    def __init__(self, user, board_user, ai, board_ai):
-        self.user = User()
-        self.board_user = user.board
-        self.ai = AI()
-        self.board_ai = user.board_2
+    def __init__(self, size=6):
+        self.size = size
+        #us_board = self.random_board()
+        #ai_board = self.random_board(hid=True)
+        #self.us = User(us_board, ai_board)
+        #self.ai = AI(ai_board, us_board)
+    def try_board(self):
+        ship_lens = [3, 2, 2, 1, 1, 1, 1]
+        board = Board()
+        attempts = 0
+        for i in ship_lens:
+            while True:
+                attempts += 1
+                if attempts > 1000:
+                    return None
+                ship = Ship(Dot(randint(0, self.size - 1), randint(0, self.size - 1)), i, randint(0, 1))
+                try:
+                    board.add_ship(ship)
+                    break
+                except BoardWrongShipExceptions:
+                    pass
+        return board
 
-ship_1 = Ship(Dot(3, 3), 2, 0)
-board_1 = Board()
-board_1.add_ship(ship_1)
-board_1.contour(ship_1)
-board_1.shot(Dot(0, 0))
-board_1.shot(Dot(3,3))
-board_1.shot(Dot(4,3))
-print(board_1)
+    #def random_board(self):
+        #board = None
+        #while board == None:
+        #    self.try_board()
+        #return board
+
+g = Game()
+print(g.try_board())
